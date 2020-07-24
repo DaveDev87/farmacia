3fad199e6de0a02e287152e3364acb246519597d
Dave <alexruedarivas@outlook.com>
2020-07-23 19:17:18 -0500

    header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/App.css";
 //Views
 import Home from "./views/Home";
 
+//Components
+import Header from "./components/Header";
+
+
 function App() {
   return (
-  <Fragment>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-      </Switch>
-    </Router>
-  </Fragment>
-  )
+    <div className="container">
+      <Router>
+        <>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/header.css";
+
+const Header = () => {
+  return (
+    <nav className="header">
+      <div>
+        <a className="logo">Farmacia</a>
+      </div>
+      <div class="header-right">
+        <a href="#home">
+          Home
+        </a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
